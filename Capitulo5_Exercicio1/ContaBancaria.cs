b9e3279e8ba491a9fea048f43495687316c10196
Adenilton Pereira <adenilton_23@yahoo.com.br>
2020-05-06 23:09:16 -0300

    Projeto deposito finalizado

---
+++ Capitulo5_Exercicio1/ContaBancaria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Capitulo5_Exercicio1
+{
+    class ContaBancaria
+    {
+        //Encapsolamento
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        // Construtor
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+        public ContaBancaria(int numero, string titular, double saldo): this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo = Saldo + quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo = Saldo - (quantia + 5.0);
+        }
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+
+
+
+
+        /* public int Numero;
+         public string Titular;
+         public double Saldo;
+
+         public void Deposito(double quantia)
+         {
+             return quantia();*/
+    }
+
+    
+}
---
+++ Capitulo5_Exercicio1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+namespace Capitulo5_Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Titular da Conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Deposito inicial (s/n) ");
+            char opcao = char.Parse(Console.ReadLine());
+            if(opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Qual o valor do deposito: ");
+                double depositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Valor do deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados Atualizados");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Valor do saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados Atualizados");
+            Console.WriteLine(conta);
+
+
+
+
+
+        }
+    }
+}
